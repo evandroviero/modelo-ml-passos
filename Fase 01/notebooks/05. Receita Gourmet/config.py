4f517458f7c6d4fa463eca004d27da08d032998c
evandroviero <evandro.viero@gmail.com>
2025-04-12 17:07:27 -0300

    flask finalizado

---
+++ Fase 01/notebooks/05. Receita Gourmet/config.py
@@ -0,0 +1,22 @@
+# class Config:
+#     se dentro do app.py precisa ser classe
+#     SECRET_KEY = "sua_chave_secreta"
+#     CACHE_TYPE = "simple"
+#     SWAGGER = {
+#         "title": "Catálogo de Receitas Gourmet",
+#         "uiversion": 3
+#     }
+#     SQLALCHEMY_DATABASE_URI = "sqlite:///recipes.db"
+#     SQLACLHEMY_TRACK_MODIFICATIONS = False
+#     JWT_SECRET_KEY = "sua_chave_jwt_secreta"
+
+
+SECRET_KEY = "sua_chave_secreta"
+CACHE_TYPE = "simple"
+SWAGGER = {
+    "title": "Catálogo de Receitas Gourmet",
+    "uiversion": 3
+}
+SQLALCHEMY_DATABASE_URI = "sqlite:///recipes.db"
+SQLACLHEMY_TRACK_MODIFICATIONS = False
+JWT_SECRET_KEY = "sua_chave_jwt_secreta"
